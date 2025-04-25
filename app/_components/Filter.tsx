a4a5c183d477d80ef0df06a56307c31f95761bf8
arkan <darkannur@gmail.com>
2025-04-26 00:01:55 +0700

    fix

--- app/_components/Filter.tsx
+++ app/_components/Filter.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
 
 type filterCode = {
   code?: string;
@@ -14,6 +15,9 @@ type Props = {
 };
 
 function Filter({ id, filterName, activeFilter, setActiveFilter }: Props) {
+  const [isClient, setIsClient] = useState(false);
+  useEffect(() => setIsClient(true), []);
+
   return (
     <div className="flex items-center h-full border rounded-full cursor-pointer bg-gradient text-sm sm:text-base text-nowrap relative overflow-hidden">
       {filterName?.map((filter) => {
@@ -29,7 +33,7 @@ function Filter({ id, filterName, activeFilter, setActiveFilter }: Props) {
                 : "py-1 px-5 text-light-50/70 hover:text-light-50 scale-105"
             }`}
           >
-            {isActive && (
+            {isActive && isActive && (
               <motion.div
                 layoutId={`filter-highlight-${id}`}
                 className="absolute inset-0 bg-opt-gradient rounded-full z-[-1] border scale-105"
