7fb58fd91e5017a9d1ec245cf1b779c242624978
arkan <darkannur@gmail.com>
2025-06-26 09:44:19 +0700

    add icon logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,6 +30,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+       <link rel="icon" href="/icon.svg" sizes="any" />
+      </head>
       <body
         className={`${roboto.variable} ${playfairDisplay.variable} font-roboto bg-dark-50 h-auto text-light-50`}
       >
